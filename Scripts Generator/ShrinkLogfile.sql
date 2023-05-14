df4aa908602982455dd7a87bb9b391cac3e54cc3
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-05-14 20:14:44 -0300

    Update ShrinkLogfile.sql

--- Scripts Generator/ShrinkLogfile.sql
+++ Scripts Generator/ShrinkLogfile.sql
@@ -8,3 +8,19 @@ sys.master_files as a
 INNER JOIN sys.databases as b
 on a.database_id = b.database_id
 WHERE a.type_desc='LOG' and b.name not in('master', 'msdb', 'tempdb', 'model');
+
+
+
+
+SELECT 
+'USE ['+b.name+']
+GO
+CHECKPOINT
+GO
+DBCC SHRINKFILE (N'''  + a.name +  ''' , 0)
+GO'
+FROM
+sys.master_files as a
+INNER JOIN sys.databases as b
+on a.database_id = b.database_id
+WHERE a.type_desc='LOG' and b.name not in('master', 'msdb', 'tempdb', 'model');
