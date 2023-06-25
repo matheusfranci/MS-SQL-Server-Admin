2092075fa267a8db3defa26ffd66a985e4d7d4e7
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-06-24 23:35:09 -0300

    Update DefragIndex.sql

--- Scripts Generator/DefragIndex.sql
+++ Scripts Generator/DefragIndex.sql
@@ -3,8 +3,8 @@ select
     nome_indice = name, 
     fragmentacao_media = avg_fragmentation_in_percent,
     script = case
-        when avg_fragmentation_in_percent > 30 then 'alter index ' + name + ' on ' + object_name(b.object_id) + ' rebuild with (online = on)'
-        when avg_fragmentation_in_percent >= 5 and avg_fragmentation_in_percent <= 30 then 'alter index ' + name + ' on ' + object_name(b.object_id) + ' reorganize'
+        when avg_fragmentation_in_percent > 30 then 'ALTER INDEX ' + name + ' ON ' + object_name(b.object_id) + ' REBUILD WITH (FILLFACTOR = 80, DATA_COMPRESSION = PAGE, MAXDOP=8, ONLINE=ON)'
     end
 from sys.dm_db_index_physical_stats (db_id('curso'), null, null, null, null) as a 
 join sys.indexes as b on a.object_id = b.object_id and a.index_id = b.index_id
+AND avg_fragmentation_in_percent > 30;
