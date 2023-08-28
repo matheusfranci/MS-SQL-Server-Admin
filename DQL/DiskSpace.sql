eb926978dc6204a487485c9b4377dcbec1b1a5a0
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-08-28 11:30:24 -0300

    Update DiskSpace.sql

--- DQL/DiskSpace.sql
+++ DQL/DiskSpace.sql
@@ -3,11 +3,69 @@ SELECT DISTINCT
     VS.volume_mount_point [Montagem] ,
     VS.logical_volume_name AS [Volume] ,
     CAST(CAST(VS.total_bytes AS DECIMAL(19, 2)) / 1024 / 1024 / 1024 AS DECIMAL(10, 2)) AS [Total (GB)] ,
-    CAST(CAST(VS.available_bytes AS DECIMAL(19, 2)) / 1024 / 1024 / 1024 AS DECIMAL(10, 2)) AS [Espaço Disponível (GB)] ,
-    CAST(( CAST(VS.available_bytes AS DECIMAL(19, 2)) / CAST(VS.total_bytes AS DECIMAL(19, 2)) * 100 ) AS DECIMAL(10, 2)) AS [Espaço Disponível ( % )] ,
-    CAST(( 100 - CAST(VS.available_bytes AS DECIMAL(19, 2)) / CAST(VS.total_bytes AS DECIMAL(19, 2)) * 100 ) AS DECIMAL(10, 2)) AS [Espaço em uso ( % )]
+    CAST(CAST(VS.available_bytes AS DECIMAL(19, 2)) / 1024 / 1024 / 1024 AS DECIMAL(10, 2)) AS [EspaÃ§o DisponÃ­vel (GB)] ,
+    CAST(( CAST(VS.available_bytes AS DECIMAL(19, 2)) / CAST(VS.total_bytes AS DECIMAL(19, 2)) * 100 ) AS DECIMAL(10, 2)) AS [EspaÃ§o DisponÃ­vel ( % )] ,
+    CAST(( 100 - CAST(VS.available_bytes AS DECIMAL(19, 2)) / CAST(VS.total_bytes AS DECIMAL(19, 2)) * 100 ) AS DECIMAL(10, 2)) AS [EspaÃ§o em uso ( % )]
 FROM
     sys.master_files AS MF
     CROSS APPLY [sys].[dm_os_volume_stats](MF.database_id, MF.file_id) AS VS
 WHERE
     CAST(VS.available_bytes AS DECIMAL(19, 2)) / CAST(VS.total_bytes AS DECIMAL(19, 2)) * 100 < 100;
+
+
+declare @svrName varchar(255)
+declare @sql varchar(400)
+--by default it will take the current server name, we can the set the server name as well
+set @svrName = @@SERVERNAME
+set @sql = 'powershell.exe -c "Get-WmiObject -ComputerName ' + QUOTENAME(@svrName,'''') + ' -Class Win32_Volume -Filter ''DriveType = 3'' | select name,capacity,freespace | foreach{$_.name+''|''+$_.capacity/1048576+''%''+$_.freespace/1048576+''*''}"'
+--creating a temporary table
+CREATE TABLE #output
+(line varchar(255))
+--inserting disk name, total space and free space value in to temporary table
+insert #output
+EXEC xp_cmdshell @sql
+--script to retrieve the values in MB from PS Script output
+select rtrim(ltrim(SUBSTRING(line,1,CHARINDEX('|',line) -1))) as drivename
+   ,round(cast(rtrim(ltrim(SUBSTRING(line,CHARINDEX('|',line)+1,
+   (CHARINDEX('%',line) -1)-CHARINDEX('|',line)) )) as Float),0) as 'capacity(MB)'
+   ,round(cast(rtrim(ltrim(SUBSTRING(line,CHARINDEX('%',line)+1,
+   (CHARINDEX('*',line) -1)-CHARINDEX('%',line)) )) as Float),0) as 'freespace(MB)'
+from #output
+where line like '[A-Z][:]%'
+order by drivename
+--script to retrieve the values in GB from PS Script output
+select rtrim(ltrim(SUBSTRING(line,1,CHARINDEX('|',line) -1))) as drivename
+   ,round(cast(rtrim(ltrim(SUBSTRING(line,CHARINDEX('|',line)+1,
+   (CHARINDEX('%',line) -1)-CHARINDEX('|',line)) )) as Float)/1024,0) as 'capacity(GB)'
+   ,round(cast(rtrim(ltrim(SUBSTRING(line,CHARINDEX('%',line)+1,
+   (CHARINDEX('*',line) -1)-CHARINDEX('%',line)) )) as Float) /1024 ,0)as 'freespace(GB)'
+from #output
+where line like '[A-Z][:]%'
+order by drivename
+--script to drop the temporary table
+drop table #output
+
+
+
+declare @svrName varchar(255)
+declare @sql varchar(400)
+--by default it will take the current server name, we can the set the server name as well
+set @svrName = @@SERVERNAME
+set @sql = 'powershell.exe -c "Get-WmiObject -ComputerName ' + QUOTENAME(@svrName,'''') + ' -Class Win32_Volume -Filter ''DriveType = 3'' | select name,capacity,freespace | foreach{$_.name+''|''+$_.capacity/1048576+''%''+$_.freespace/1048576+''*''}"'
+--creating a temporary table
+CREATE TABLE #output
+(line varchar(255))
+--inserting disk name, total space and free space value in to temporary table
+insert #output
+EXEC xp_cmdshell @sql
+--script to retrieve the values in GB from PS Script output
+select rtrim(ltrim(SUBSTRING(line,1,CHARINDEX('|',line) -1))) as drivename
+   ,round(cast(rtrim(ltrim(SUBSTRING(line,CHARINDEX('|',line)+1,
+   (CHARINDEX('%',line) -1)-CHARINDEX('|',line)) )) as Float)/1024,0) as 'capacity(GB)'
+   ,round(cast(rtrim(ltrim(SUBSTRING(line,CHARINDEX('%',line)+1,
+   (CHARINDEX('*',line) -1)-CHARINDEX('%',line)) )) as Float) /1024 ,0)as 'freespace(GB)'
+from #output
+where line like '[A-Z][:]%'
+order by drivename
+--script to drop the temporary table
+drop table #output
