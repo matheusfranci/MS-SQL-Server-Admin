09e4395c9b6232bb1cf91dd1cd4cf1d5ef420485
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-09-03 02:57:54 -0300

    Update CheckError.sql

--- DQL/CheckError.sql
+++ DQL/CheckError.sql
@@ -13,3 +13,20 @@ EXEC master.dbo.xp_readerrorlog
      @searchString2, 
      @start, 
      @end;
+
+
+DECLARE @logFileType SMALLINT= 1;
+DECLARE @start DATETIME;
+DECLARE @end DATETIME;
+DECLARE @logno INT= 0;
+SET @start = '2023-09-02 00:00:00.000';
+SET @end = '2023-09-03 00:00:00.000';
+DECLARE @searchString1 NVARCHAR(256)= 'DBCC CHECKDB';
+DECLARE @searchString2 NVARCHAR(256)= 'DBCC CHECKDB';
+EXEC master.dbo.xp_readerrorlog 
+     @logno, 
+     @logFileType, 
+     @searchString1, 
+     @searchString2, 
+     @start, 
+     @end;
