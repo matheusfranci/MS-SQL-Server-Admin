c10d15e6ad55c3573354c25b304c7054f00af933
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-09-03 07:58:45 -0300

    Update CheckCommandLog.sql

--- MaintenanceJobs/CheckCommandLog.sql
+++ MaintenanceJobs/CheckCommandLog.sql
@@ -1,17 +1,26 @@
 -- Check Index
-SELECT *
+SELECT
+DATABASENAME,
+SCHEMANAME,
+OBJECTNAME,
+INDEXNAME,
+COMMAND,
+FORMAT (StartTime, 'dd/MM/yyyy hh:mm:ss') AS Inicio,
+FORMAT (EndTime, 'dd/MM/yyyy hh:mm:ss') AS Fim,
+datediff (minute, StartTime, EndTime) AS "Duração"
 FROM
 CommandLog
-WHERE CommandType = 'ALTER_INDEX'
+WHERE CommandType = 'ALTER_INDEX';
 
 
 
 -- Check checkdb job
-SELECT databasename,
-command,
-StartTime,
-EndTime,
-datediff (minute, StartTime, EndTime) AS Duration,
+SELECT databasename AS "Banco de dados",
+command AS Comando,
+FORMAT (StartTime, 'dd/MM/yyyy hh:mm:ss') AS Inicio,
+FORMAT (EndTime, 'dd/MM/yyyy hh:mm:ss') AS Fim,
+datediff (minute, StartTime, EndTime) AS "Duração",
 ErrorNumber,
 ErrorMessage
-FROM CommandLog WHERE CommandType='DBCC_CHECKDB'
+FROM ORION.dbo.CommandLog WHERE CommandType='DBCC_CHECKDB'
+
