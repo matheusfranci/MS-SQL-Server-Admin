cb3be975a761ad3d0b86a256828314502444186e
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-09-03 00:15:31 -0300

    Update CreateUserWithRole.sql

--- Scripts Generator/CreateUserWithRole.sql
+++ Scripts Generator/CreateUserWithRole.sql
@@ -1,24 +1,5 @@
-CREATE TABLE #tmproleresult
-(
-Comando VARCHAR(MAX)
-)
-INSERT INTO #tmproleresult
-EXEC master.sys.sp_MSforeachdb' 
-USE [?]
-SELECT "USE ["+ DB_NAME() +"]
-GO
-ALTER ROLE ["+ DBRole.NAME +"] ADD MEMBER ["+ DBUser.NAME +"]
-GO" AS Comando
-FROM sys.database_principals DBUser
-INNER JOIN sys.database_role_members DBM ON DBM.member_principal_id = DBUser.principal_id
-INNER JOIN sys.database_principals DBRole ON DBRole.principal_id = DBM.role_principal_id
-WHERE DBUser.name = "S2\phillipepinheiro"
-'
-SELECT * FROM #tmproleresult
-DROP TABLE #tmproleresult
-
-
-
+-- Usuário precisa ser criado na instância antes.
+-- Criação  da tabela temporária para armazenar as ddl's dos usuário nos bancos
 CREATE TABLE #tmpuserresult
 (
 Comando VARCHAR(MAX)
@@ -29,8 +10,37 @@ USE [?]
 SELECT "
 USE ["+ DB_NAME() +"]
 GO
-CREATE USER [" + NAME + "] FOR LOGIN [" + NAME + "]" AS "--Database Users Creation--"
+CREATE USER [ usuariodestinoaqui ] FOR LOGIN [ usuariodestinoaqui ]" AS "--Database Users Creation--"
 FROM sys.database_principals
-WHERE NAME = "S2\phillipepinheiro" '
-SELECT * FROM #tmpuserresult
-DROP TABLE #tmpuserresult
+WHERE NAME = "POLIEDRO\JNMOURA.MARCUS.SOUZA" ' -- Usuário de exemplo muito provavelmente utilzado como parâmetro na abertura do chamado
+SELECT * FROM #tmpuserresult -- Extraia os comandos aqui e execute
+DROP TABLE #tmpuserresult -- Apague a tabela temporária
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Criação da tabela temporária para armazenamento das dcl's
+CREATE TABLE #grantresult
+(
+role_principal_name VARCHAR(MAX),
+member_principal_name VARCHAR(MAX),
+Banco VARCHAR(MAX)
+)
+INSERT INTO #grantresult -- Inserção das database roles atribuídas a determinado usuário de exemplo que está no chamado
+EXEC master.sys.sp_MSforeachdb 
+'USE [?]
+SELECT r.name role_principal_name, m.name AS member_principal_name, DB_NAME() AS Banco
+FROM sys.database_role_members rm 
+JOIN sys.database_principals r 
+    ON rm.role_principal_id = r.principal_id
+JOIN sys.database_principals m 
+    ON rm.member_principal_id = m.principal_id
+where m.name IN ("POLIEDRO\JNMOURA.MARCUS.SOUZA")
+order by m.name;'
+-- Permissão 
+SELECT '
+USE ['+ t.Banco +']
+GO
+ALTER ROLE [ '+ t.role_principal_name +' ] ADD MEMBER [ usuariox ]
+GO'
+FROM #grantresult t
+DROP TABLE #grantresult -- Apague a tabela temporária
+
