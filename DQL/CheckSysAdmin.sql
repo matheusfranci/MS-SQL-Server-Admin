f1e259564f37053a90bae4b624d933d72f980bde
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-12-06 11:20:23 -0300

    Update CheckSysAdmin.sql

--- DQL/CheckSysAdmin.sql
+++ DQL/CheckSysAdmin.sql
@@ -2,3 +2,14 @@ SELECT   name,type_desc,is_disabled
 FROM     master.sys.server_principals 
 WHERE    IS_SRVROLEMEMBER ('sysadmin',name) = 1
 ORDER BY name
+
+
+SELECT   name,type_desc,
+CASE
+WHEN is_disabled = 1 then 'Desabilidado'
+WHEN is_disabled = 0 then 'Habilitado'
+ELSE 'Não classificado'
+END AS 'Status'
+FROM     master.sys.server_principals WITH(NOLOCK)
+WHERE    IS_SRVROLEMEMBER ('sysadmin',name) = 1
+ORDER BY name;
