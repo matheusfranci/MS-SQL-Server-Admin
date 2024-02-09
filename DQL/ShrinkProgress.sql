cf6a093cfa61683f0eb946c7ab41a663045bfc4e
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2024-02-09 11:06:37 -0300

    Update ShrinkProgress.sql

--- DQL/ShrinkProgress.sql
+++ DQL/ShrinkProgress.sql
@@ -11,3 +11,18 @@ percent_complete
 from  sys.dm_exec_requests
 where estimated_completion_time > 1
 order by total_elapsed_time desc
+
+
+select
+[status],
+start_time as "Início",
+convert(varchar,(total_elapsed_time/(1000))/60) + 'M ' + convert(varchar,(total_elapsed_time/(1000))%60) + 'S' AS [Tempo de processamento],
+convert(varchar,(estimated_completion_time/(1000))/60) + 'M ' + convert(varchar,(estimated_completion_time/(1000))%60) + 'S' as [Estimativa em min],
+command as "Comando",
+last_wait_type,
+database_id,
+blocking_session_id as "Sessão bloqueadora",
+percent_complete as "Percentual completo"
+from  sys.dm_exec_requests
+where estimated_completion_time > 1
+order by total_elapsed_time desc
