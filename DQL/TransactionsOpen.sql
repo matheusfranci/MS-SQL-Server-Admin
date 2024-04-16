713930a6833606c89c1ccf122581b1d4b5e3d67a
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2024-04-16 16:42:14 -0300

    Create TransactionsOpen.sql

---
+++ DQL/TransactionsOpen.sql
@@ -0,0 +1,2 @@
+-- Another command to look at is DBCC OPENTRAN. This will show you if you have any open transactions in your transaction log that have not completed or have not been committed
+DBCC OPENTRAN
