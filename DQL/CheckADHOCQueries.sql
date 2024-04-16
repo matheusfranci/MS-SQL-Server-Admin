c334f269d46fbe04c74853e548d03c8d1c8c6f82
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2024-04-16 17:11:48 -0300

    Update CheckADHOCQueries.sql

--- DQL/CheckADHOCQueries.sql
+++ DQL/CheckADHOCQueries.sql
@@ -1,6 +1,14 @@
-
 SELECT Convert(INT,
                 Sum(CASE p.objtype WHEN 'Adhoc' THEN 1 ELSE 0 END) * 1.00
                 / Count(*) * 100
               )
   FROM sys.dm_exec_cached_plans AS p
+
+SELECT Convert(INT,Sum
+        (
+        CASE a.objtype 
+        WHEN 'Adhoc' 
+        THEN 1 ELSE 0 END)
+        * 1.00/ Count(*) * 100
+              ) as 'Ad-hoc query %'
+  FROM sys.dm_exec_cached_plans AS a
