f5d5e7779d32e0c08f66431b6869762187a251b2
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2024-04-19 09:36:09 -0300

    Create LastLogBackup.sql

---
+++ DQL/LastLogBackup.sql
@@ -0,0 +1,16 @@
+SELECT   d.name,
+         d.recovery_model_desc,
+         MAX(b.backup_finish_date) AS backup_finish_date,
+CASE 
+WHEN b.type = 'L'
+THEN 'Log Backup'
+else 'Null'
+END AS 'Backup_type'
+FROM     master.sys.databases d
+         LEFT OUTER JOIN msdb..backupset b
+         ON       b.database_name = d.name
+         AND      b.type          = 'L'
+WHERE d.state_desc = 'ONLINE'
+AND d.recovery_model_desc = 'FULL'
+GROUP BY d.name, d.recovery_model_desc, b.type
+ORDER BY backup_finish_date DESC
